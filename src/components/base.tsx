3307b1d9b5d53faa2e0544677c400ffcb976f32d
C4illin <emrik.ostling@gmail.com>
2024-05-26 14:42:19 +0200

    fix picocss

--- src/components/base.tsx
+++ src/components/base.tsx
@@ -3,7 +3,7 @@ export const BaseHtml = ({ children, title = "ConvertX" }) => (
     <head>
       <meta charset="UTF-8" />
       <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-      <title>{title}</title>
+      <title safe>{title}</title>
       <link rel="stylesheet" href="/pico.lime.min.css" />
       <link rel="stylesheet" href="/style.css" />
       <link
