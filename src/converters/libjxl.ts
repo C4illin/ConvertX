ff680cb29534a25c3148a90fd064bb86c71fb482
C4illin <emrik.ostling@gmail.com>
2024-06-11 19:30:06 +0200

    feat: add libjxl for jpegxl conversion

---
+++ src/converters/libjxl.ts
@@ -0,0 +1,71 @@
+import { exec } from "node:child_process";
+
+// declare possible conversions
+export const properties = {
+  from: {
+    jxl: ["jxl"],
+    images: [
+      "apng",
+      "exr",
+      "gif",
+      "jpeg",
+      "pam",
+      "pfm",
+      "pgm",
+      "pgx",
+      "png",
+      "ppm",
+    ],
+  },
+  to: {
+    jxl: [
+      "apng",
+      "exr",
+      "gif",
+      "jpeg",
+      "pam",
+      "pfm",
+      "pgm",
+      "pgx",
+      "png",
+      "ppm",
+    ],
+    images: ["jxl"],
+  },
+};
+
+export function convert(
+  filePath: string,
+  fileType: string,
+  convertTo: string,
+  targetPath: string,
+  // biome-ignore lint/suspicious/noExplicitAny: <explanation>
+  options?: any,
+): Promise<string> {
+  let tool = "";
+  if (fileType === "jxl") {
+    tool = "djxl";
+  }
+
+  if (convertTo === "jxl") {
+    tool = "cjxl";
+  }
+
+  return new Promise((resolve, reject) => {
+    exec(`${tool} "${filePath}" "${targetPath}"`, (error, stdout, stderr) => {
+      if (error) {
+        reject(`error: ${error}`);
+      }
+
+      if (stdout) {
+        console.log(`stdout: ${stdout}`);
+      }
+
+      if (stderr) {
+        console.error(`stderr: ${stderr}`);
+      }
+
+      resolve("success");
+    });
+  });
+}
