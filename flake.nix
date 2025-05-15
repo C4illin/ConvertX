bba420386faf54be6fac3a0009856ec10bc0f610
C4illin <emrik.ostling@gmail.com>
2025-05-15 22:11:30 +0200

    chore: flake wip

--- flake.nix
+++ flake.nix
@@ -8,55 +8,53 @@
 
   outputs = { self, nixpkgs, flake-utils, ... }:
     flake-utils.lib.eachDefaultSystem (system:
-      let
-        pkgs = import nixpkgs { inherit system; };
-        appSrc = ./.;
+      let pkgs = import nixpkgs { inherit system; };
+      appSrc = ./.;
 
-        app = pkgs.dockerTools.buildLayeredImage {
-          name = "convertx";
-          tag = "latest";
+      app = pkgs.dockerTools.buildLayeredImage {
+        name = "convertx";
+        tag = "latest";
 
-          contents = [
-            pkgs.bun
-            pkgs.resvg
-            pkgs.ffmpeg
-            pkgs.graphicsmagick
-            pkgs.ghostscript
-            pkgs.vips
-            pkgs.pandoc
-            pkgs.texlive.combined.scheme-full
-            pkgs.calibre
-            pkgs.inkscape
-            pkgs.poppler_utils
-            pkgs.assimp
-            pkgs.jxrlib
-            pkgs.libheif
-            pkgs.libjxl
-            pkgs.python3Packages.numpy
+        contents = [
+          pkgs.bun
+          pkgs.resvg
+          pkgs.ffmpeg
+          pkgs.graphicsmagick
+          pkgs.ghostscript
+          pkgs.vips
+          pkgs.pandoc
+          pkgs.texlive.combined.scheme-full
+          pkgs.calibre
+          pkgs.inkscape
+          pkgs.poppler_utils
+          pkgs.assimp
+          pkgs.jxrlib
+          pkgs.libheif
+          pkgs.libjxl
+          pkgs.python3Packages.numpy
+        ];
+
+        config = {
+          Env = [
+            "NODE_ENV=production"
+            "PATH=/bin:/usr/bin"
           ];
-
-          config = {
-            Env = [
-              "NODE_ENV=production"
-              "PATH=/bin:/usr/bin"
-            ];
-            WorkingDir = "/app";
-            Cmd = [ "bun" "run" "./src/index.tsx" ];
-            ExposedPorts = {
-              "3000/tcp" = {};
-            };
+          WorkingDir = "/app";
+          Cmd = [ "bun" "run" "./src/index.tsx" ];
+          ExposedPorts = {
+            "3000/tcp" = {};
           };
-
-          extraCommands = ''
-            export PATH=${pkgs.bun}/bin:$PATH
-            mkdir -p app
-            cp -r ${appSrc}/* app/
-            cd app
-            bun install --frozen-lockfile --production
-            bun run build
-          '';
         };
+
+        extraCommands = ''
+          export PATH=${pkgs.bun}/bin:$PATH
+          mkdir -p /app
+          cp -r ${./dist}/* /app/
+        '';
+      };
+
       in {
         packages.default = app;
-      });
+      }
+    );
 }
