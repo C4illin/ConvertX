2ce3fee70be262a9e9daa558b70e89a23cb395f2
Maxime <m.behm@faun.fr>
2025-06-25 14:34:27 +0200

    added tar creation when using Download All

--- public/results.js
+++ public/results.js
@@ -1,18 +1,4 @@
 const webroot = document.querySelector("meta[name='webroot']").content;
-
-window.downloadAll = function () {
-  // Get all download links
-  const downloadLinks = document.querySelectorAll("a[download]");
-
-  // Trigger download for each link
-  downloadLinks.forEach((link, index) => {
-    // We add a delay for each download to prevent them from starting at the same time
-    setTimeout(() => {
-      const event = new MouseEvent("click");
-      link.dispatchEvent(event);
-    }, index * 100);
-  });
-};
 const jobId = window.location.pathname.split("/").pop();
 const main = document.querySelector("main");
 let progressElem = document.querySelector("progress");
