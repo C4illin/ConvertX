7f9c8868fd3690d8c1e96dabcd938c56e3580b41
Jörg Krzeslak <selanne08@gmx.de>
2025-07-24 13:18:16 +0200

    test: add unit test for graphicsmagick.ts

---
+++ tests/converters/graphicsmagick.test.ts
@@ -0,0 +1,19 @@
+import { test } from "bun:test";
+import { convert } from "../../src/converters/graphicsmagick.ts";
+import {
+  runConvertFailTest,
+  runConvertLogsStderror,
+  runConvertSuccessTest,
+} from "./helpers/converters.ts";
+
+test("convert resolves when execFile succeeds", async () => {
+  await runConvertSuccessTest(convert);
+});
+
+test("convert rejects when execFile fails", async () => {
+  await runConvertFailTest(convert);
+});
+
+test("convert logs stderr when present", async () => {
+  await runConvertLogsStderror(convert);
+});
