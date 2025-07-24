3561516341dc3224a64501e9a2e068ba239b66b8
Jörg Krzeslak <selanne08@gmx.de>
2025-07-24 21:08:10 +0200

    feat: extract duplicate code into another helper

--- tests/converters/resvg.test.ts
+++ tests/converters/resvg.test.ts
@@ -1,24 +1,7 @@
 import { test } from "bun:test";
 import { convert } from "../../src/converters/resvg.ts";
-import {
-  runConvertFailTest,
-  runConvertLogsStderror,
-  runConvertLogsStderrorAndStdout,
-  runConvertSuccessTest,
-} from "./helpers/converters.ts";
+import { runCommonTests } from "./helpers/commonTests.ts";
 
-test("convert resolves when execFile succeeds", async () => {
-  await runConvertSuccessTest(convert);
-});
+runCommonTests(convert);
 
-test("convert rejects when execFile fails", async () => {
-  await runConvertFailTest(convert);
-});
-
-test("convert logs stderr when present", async () => {
-  await runConvertLogsStderror(convert);
-});
-
-test("convert logs both stderr and stdout when present", async () => {
-  await runConvertLogsStderrorAndStdout(convert);
-});
+test.skip("dummy - required to trigger test detection", () => {});
