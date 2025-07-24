5957873534c823db82966a8309607da10ecedc84
Jörg Krzeslak <selanne08@gmx.de>
2025-07-24 14:35:42 +0200

    test: add unit test for libheif.ts

--- src/converters/libheif.ts
+++ src/converters/libheif.ts
@@ -1,4 +1,5 @@
-import { execFile } from "child_process";
+import { execFile as execFileOriginal } from "child_process";
+import { ExecFileFn } from "./types.ts";
 
 export const properties = {
   from: {
@@ -14,8 +15,8 @@ export function convert(
   fileType: string,
   convertTo: string,
   targetPath: string,
-  // eslint-disable-next-line @typescript-eslint/no-unused-vars
   options?: unknown,
+  execFile: ExecFileFn = execFileOriginal, // to make it mockable
 ): Promise<string> {
   return new Promise((resolve, reject) => {
     execFile("heif-convert", [filePath, targetPath], (error, stdout, stderr) => {
