5b6adccf4e26165e8ec5e3dda73011edd0117dff
Orban Gergo <ogergo99@gmail.com>
2026-08-15 22:53:41 +0300

    Added plus args for files with cover img.

--- src/converters/ffmpeg.ts
+++ src/converters/ffmpeg.ts
@@ -699,6 +699,15 @@ export async function convert(
   let extraArgs: string[] = [];
   let message = "Done";
 
+  const audioFormatsWithCover = ["m4a", "mp3", "flac", "ogg"];
+  const audioFormatsNoCover = ["wav", "aac"];
+
+  if (audioFormatsWithCover.includes(convertTo)) {
+    extraArgs.push("-c:v", "copy", "-disposition:v", "attached_pic");
+  } else if (audioFormatsNoCover.includes(convertTo)) {
+    extraArgs.push("-vn");
+  }
+
   if (convertTo === "ico") {
     // Make sure image is 256x256 or smaller
     extraArgs = [
