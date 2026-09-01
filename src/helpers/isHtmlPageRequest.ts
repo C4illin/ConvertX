7540de4eb93267e4a9b9f8d0245e674d5fd5d5a0
Justin Shetty <jas0504@gmail.com>
2026-09-01 00:29:04 -0700

    handle mixed case test/HTML

--- src/helpers/isHtmlPageRequest.ts
+++ src/helpers/isHtmlPageRequest.ts
@@ -1,4 +1,4 @@
 export function isHtmlPageRequest(request: Request): boolean {
-  const acceptsHtml = request.headers.get("accept")?.includes("text/html");
+  const acceptsHtml = request.headers.get("accept")?.toLowerCase().includes("text/html");
   return (request.method === "GET" || request.method === "HEAD") && Boolean(acceptsHtml);
 }
