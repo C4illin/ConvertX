7439834ee6e681bbe8651254f5bfba135db6abb6
Emrik Östling <emrik.ostling@gmail.com>
2026-09-26 02:11:31 +0200

    not allowed to set PGID without PUID

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 set -e
 
-if [[ -n "$PUID" && ! "$PUID" =~ ^[0-9]+$ ]] || [[ -n "$PGID" && ! "$PGID" =~ ^[0-9]+$ ]]; then
+if [[ -n "$PUID" && ! "$PUID" =~ ^[0-9]+$ ]] || [[ -n "$PGID" && ! "$PGID" =~ ^[0-9]+$ ]] || [[ -z "$PUID" && -n "$PGID" ]]; then 
   echo "[entrypoint] ERROR: PUID and PGID must be numeric integers or unset." >&2
   exit 1
 fi
